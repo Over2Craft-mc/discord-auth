7b12db2c1cd6f42551e25e12c684d4ab7539dc18
magrigry <>
2021-08-29 19:51:37 +0200

    Typo

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -3,5 +3,6 @@
 return [
     'guild_must_join' => 'You must join our Discord server before registering with Discord.',
     'login_via_discord' => 'Login with Discord',
-    'plugin_name' => 'discord-auth'
+    'plugin_name' => 'discord-auth',
+    'register-username' => 'End your registration'
 ];
--- resources/lang/fr/messages.php
+++ resources/lang/fr/messages.php
@@ -3,5 +3,6 @@
 return [
     'guild_must_join' => 'Vous devez d\'abord rejoindre notre serveur Discord avant de vous inscrire avec Discord. ',
     'login_via_discord' => 'Se connecter avec Discord   ',
-    'plugin_name' => 'discord-auth'
+    'plugin_name' => 'discord-auth',
+    'register-username' => 'Finaliser l`\'inscription'
 ];
